f86d63b568da075c1e884eaa4411bf36caf61dba
eduardo <bobmaslow5941@hotmail.com>
2022-09-24 13:46:57 -0300

    criando Controller linkado com Scenne Builder

---
+++ src/gui/ViewControler.java
@@ -0,0 +1,16 @@
+package gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ViewControler {
+	
+	@FXML
+	public Button botaoAlerta;
+	
+	@FXML
+	public void onActionButton() {
+		System.out.println("teste");
+	}
+	
+}
---
+++ src/module-info.java
@@ -0,0 +1,7 @@
+module javaFxException {
+	requires javafx.controls;
+	requires javafx.fxml;
+	
+	opens application to javafx.graphics, javafx.fxml;
+	opens gui to javafx.graphics, javafx.fxml;
+}
